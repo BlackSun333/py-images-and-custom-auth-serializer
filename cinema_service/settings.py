91b18610f94b106b69a36f4c5ea71e77d2bbcff4
Stanislav <staskobetc@gmail.com>
2026-06-08 03:17:22 +0300

    Solution

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -137,3 +137,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "user.User"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
